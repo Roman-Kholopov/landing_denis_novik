0831a937ef03f8ab66d4687171a39eab3947afc5
Roman Kholopov <roma.kholopov@gmail.com>
2022-11-25 18:52:44 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// const nav_contacts = document.querySelector('.contacts_link');
+
+// nav_contacts.addEventListener('click', () => {
+//     console.log('contacts_link')
+// })
+
+// const anchors = document.querySelectorAll('a[href*="#"]')
+
+// for (let anchor of anchors) {
+//   anchor.addEventListener('click', (e) => {
+//     e.preventDefault()
+    
+//     const blockID = anchor.getAttribute('href').substr(1)
+    
+//     document.getElementById(blockID).scrollIntoView({
+//       behavior: 'smooth',
+//       block: 'start'
+//     })
+//   })
+// }
